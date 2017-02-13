676c33cab52f5bd10cf221c7a01a668bf29457df
kalininskiy <vdm.ivanhoe@s-vfu.ru>
2017-02-13 13:10:26 +0900

    x-o clear solution

--- x-o-referee.js
+++ x-o-referee.js
@@ -1,10 +1,27 @@
 "use strict";
 
 function xoReferee(data) {
-    let index = [0, 1, 2];
+    let array = [];
+    let size = data.length;
     let result = "D";
+    for (let string of data) array.push(string.split("")); // string lines -> array
 
-    return "D" || "X" || "O";
+    function isWin(symbol) {
+        for (let i in data) {
+            let verticalSum = 0, horizontalSum = 0, crossSumLeft = 0, crossSumRight = 0;
+            for (let j in data) {
+                if (array[i][j] == symbol) verticalSum++;
+                if (array[j][i] == symbol) horizontalSum++;
+                if (array[j][j] == symbol) crossSumLeft++;
+                if (array[j][(size - 1) - j] == symbol) crossSumRight++;
+            }
+            if ((verticalSum == size) || (horizontalSum == size) || (crossSumLeft == size)|| (crossSumRight == size)) return true;
+        }
+    }
+
+    if (isWin("X")) result = "X";
+    else if (isWin("O")) result = "O";
+    return result;
 }
 
 var assert = require('assert');
